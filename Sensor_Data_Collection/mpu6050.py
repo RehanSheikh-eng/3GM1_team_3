358745a2d242c7ad25b220a98fc9f46b03dac1a5
Rehan Sheikh <rs2126@cam.ac.uk>
2023-05-11 13:19:21 +0100

    Add Accelerometer class with data collection and JSON output
    This commit adds an `Accelerometer` class that interfaces with an MPU6050 accelerometer. The class can start and stop data collection from the sensor. When data collection is started, sensor values are continuously collected every second and output as a JSON string. This includes the accelerometer and gyroscope readings, as well as a timestamp. The JSON output makes it easy to interface with other modules or external systems.

---
+++ Sensor_Data_Collection/mpu6050.py
@@ -0,0 +1,62 @@
+from machine import I2C, Pin
+import mpu6050
+import time
+import ujson
+
+class Accelerometer:
+    """
+    A class used to represent an MPU6050 Accelerometer.
+
+    ...
+
+    Attributes
+    ----------
+    i2c : I2C
+        an I2C object for communication
+    sensor : mpu6050
+        an mpu6050 accelerometer object
+    running : bool
+        a flag indicating whether data collection is currently running
+
+    Methods
+    -------
+    start():
+        Starts data collection from the accelerometer.
+    stop():
+        Stops data collection from the accelerometer.
+    """
+
+    def __init__(self, sda_pin=8, scl_pin=9):
+        """
+        Constructs all the necessary attributes for the accelerometer object.
+
+        Parameters
+        ----------
+        sda_pin : int, optional
+            the GPIO pin number used for the I2C data line (SDA)
+        scl_pin : int, optional
+            the GPIO pin number used for the I2C clock line (SCL)
+        """
+        self.i2c = I2C(1, sda=Pin(sda_pin), scl=Pin(scl_pin))
+        self.sensor = mpu6050.accel(self.i2c)
+        self.running = False
+
+    def start(self):
+        """
+        Starts data collection from the accelerometer. The data is printed as a JSON string with the following keys:
+        'AcX', 'AcY', 'AcZ', 'Tmp', 'GyX', 'GyY', 'GyZ', and 'timestamp'.
+
+        Data collection continues until the stop() method is called.
+        """
+        self.running = True
+        while self.running:
+            values = self.sensor.get_values()
+            values['timestamp'] = time.time()
+            print(ujson.dumps(values))
+            time.sleep(1)
+
+    def stop(self):
+        """
+        Stops data collection from the accelerometer.
+        """
+        self.running = False
