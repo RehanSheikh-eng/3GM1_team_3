0b545bf9dcc10c12227ea037aa8647a72cbaba3c
ahmed-ibrahim0 <ahmedibrahim6479@gmail.com>
2023-05-15 15:57:29 +0100

    Crash detection threshold

--- Sensor_Data_Collection/crash_prevention.py
+++ Sensor_Data_Collection/crash_prevention.py
@@ -2,10 +2,17 @@ import machine
 import vl53l0x
 
 class DistanceSensor:
-    def __init__(self):
-        i2c = machine.I2C(0, scl=machine.Pin(5), sda=machine.Pin(4))
+    def __init__(self, scl = 5, sda = 4):
+        i2c = machine.I2C(0, scl, sda)
         self.sensor = vl53l0x.VL53L0X(i2c)
         self.sensor.start()
         
     def get_distance(self):
         return self.sensor.read()
+
+
+def start_crash_prevention():
+    IR_distance = DistanceSensor(5,4)
+    
+    if IR_distance.get_distance < 10: # ADJUST THRESHOLD
+        # TURN ON BRAKES
