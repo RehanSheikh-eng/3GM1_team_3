Fixed distance calculation

Sensor_Data_Collection/Distance_integration.py
@@ -72,20 +72,25 @@ def calculate_distance(accel, gps, t=1):
     distance_accel = 0
     distance_GPS = 0
     prev_gps_position = gps.get_data()
+    prev_acc_data = accel.get_corrected_values()
+
     # Variance values for Kalman Filtering
-    sigma = 1
+    sigma = 1 # SET THRESHOLD THORUGH TESTING
     var_accel = sigma # Accelerometer variance should be increased with time
     var_GPS = sigma
 
     while True:
         time.sleep(t)
 
-        acc_data = accel.get_corrected_values()  # Get accelerometer data
+        acc_data = accel.get_corrected_values()  # Get accelerometer data IMPLEMENT THIS 
+
+        prev_acc_x, prev_acc_y, prev_acc_z = prev_acc_data['AcX'], prev_acc_data['AcY'], prev_acc_data['AcZ']
         acc_x, acc_y, acc_z = acc_data['AcX'], acc_data['AcY'], acc_data['AcZ']
 
-        vel_x[1] = vel_x[0] + (0.5 * t * (acc_x + acc_x))
-        vel_y[1] = vel_y[0] + (0.5 * t * (acc_y + acc_y))
-        vel_z[1] = vel_z[0] + (0.5 * t * (acc_z + acc_z))
+        # Integrating the acceleromater data
+        vel_x[1] = vel_x[0] + (0.5 * t * (prev_acc_x + acc_x))
+        vel_y[1] = vel_y[0] + (0.5 * t * (prev_acc_y + acc_y))
+        vel_z[1] = vel_z[0] + (0.5 * t * (prev_acc_z + acc_z))
 
         vel_mag[1] = np.sqrt(vel_x[1]**2 + vel_y[1]**2 + vel_z[1]**2)
 
@@ -93,19 +98,18 @@ def calculate_distance(accel, gps, t=1):
 
         curr_gps_position = gps.get_data()  # Get GPS data
         increment_distance_gps = gps.get_relative_position(prev_gps_position, curr_gps_position) # Calculate the distance traveled between two gps points
-
+        
         distance_GPS += increment_distance_gps # Distance calculated from GPS
         distance_fused = (var_GPS*distance_accel + var_accel*distance_GPS) / (var_accel + var_GPS) # Kalman filtered distance
 
-        vel_x[0] = vel_x[1]
-        vel_y[0] = vel_y[1]
-        vel_z[0] = vel_z[1]
+        # Shift data timestep
+        prev_acc_data = acc_data
         vel_mag[0] = vel_mag[1]
+        prev_gps_position = curr_gps_position
 
-    if var_accel < 100: # to prevent numerical overflow
-        var_accel += 1 # to account for drift
-    if vel_mag[0] < 0.001:
-        var_accel = sigma # variance resets when wheelchair is stationary
-    
-    idx += 1
-    on = False
+
+        if var_accel < 100: # to prevent numerical overflow
+            var_accel += 1 # to account for drift
+
+        if vel_mag[0] < 0.001: # SET THRESHOLD THROUGH TESTING
+            var_accel = sigma # variance resets when wheelchair is stationary