f9c5ec4111ad882fdbd52dbce5ce64527fa71764
ahmed-ibrahim0 <ahmedibrahim6479@gmail.com>
2023-05-16 14:34:50 +0100

    Working crash prevention code

--- Sensor_Data_Collection/crash_prevention.py
+++ Sensor_Data_Collection/crash_prevention.py
@@ -1,18 +1,36 @@
-import machine
-import vl53l0x
+from machine import I2C, Pin
+import VL53L0X as vl
 
 class DistanceSensor:
-    def __init__(self, scl = 5, sda = 4):
-        i2c = machine.I2C(0, scl, sda)
-        self.sensor = vl53l0x.VL53L0X(i2c)
+    '''
+    Class to receive distance data (in mm) from the VL53L0X infrared sensor
+    Note: accuracy of sensor is low above 500mm
+    '''
+    def __init__(self, id, sda, scl):
+        self.i2c = I2C(id = id, sda = Pin(sda), scl = Pin(scl))
+        self.sensor = vl.VL53L0X(self.i2c)
         self.sensor.start()
-        
+    
     def get_distance(self):
         return self.sensor.read()
-
-
-def start_crash_prevention():
-    IR_distance = DistanceSensor(5,4)
     
-    if IR_distance.get_distance < 10: # ADJUST THRESHOLD
-        # TURN ON BRAKES
+    def get_smooth_distance(self, MA_order = 3): # Computes the calibrated moving average (default = 3-point moving average)
+        self.average_distance = 0
+        for i in range(MA_order):
+            self.average_distance += self.sensor.read()
+        return ((int(self.average_distance) / MA_order) - 52) # Offset needed for calibration
+    
+    def print_distance(self):
+        while True:
+            print("Distance:", self.get_smooth_distance(), "mm")
+    
+    def start_crash_prevention(self, MA_order = 3):
+        while True:
+            if self.get_smooth_distance(MA_order) < 200:
+                print("CRASH RISK!")
+                break
+                # REPLACE WITH CODE TO TURN ON BRAKES/TURN OFF MOTOR
+        
+    
+distance_sensor = DistanceSensor(id = 0, sda = 0, scl = 1)
+distance_sensor.start_crash_prevention()
