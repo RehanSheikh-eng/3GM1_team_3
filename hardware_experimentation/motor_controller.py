2113d255384fcb63bac6a492f84499e8b9525704
T-Robson <timfrobson@gmail.com>
2023-05-22 12:09:17 +0100

    noice

--- hardware_experimentation/motor_controller.py
+++ hardware_experimentation/motor_controller.py
@@ -3,11 +3,7 @@ from machine import Pin, PWM
 
 class Motor:
 
-    def __init__(self, r_en_pin, l_en_pin, RPWM_pin, LPWM_pin, brake_pin, PWM_freq = 2000):
-
-        self._forward_enable = Pin(r_en_pin, Pin.OUT)
-
-        self._reverse_enable = Pin(l_en_pin, Pin.OUT)
+    def __init__(self, RPWM_pin, LPWM_pin, brake_pin, PWM_freq = 2000):
 
         self._forward_pwm = PWM(Pin(RPWM_pin)) #, PWM_freq, 0) #freq=PWM_freq, duty_u16=0)
         
@@ -23,8 +19,6 @@ class Motor:
     def disable(self):
         self._enabled = False
         self._brake_relay.off()
-        self._forward_enable.off()
-        self._reverse_enable.off()
         self._forward_pwm.duty_u16(0)
         self._reverse_pwm.duty_u16(0)
 
@@ -35,12 +29,8 @@ class Motor:
         else:
             pwm_value = int(abs(set_value)*65535)    
             if set_value > 0:
-                self._forward_enable.value(True)
-                self._reverse_enable.value(False)
                 self._forward_pwm.duty_u16(pwm_value)
                 self._reverse_pwm.duty_u16(0)
             elif set_value < 0:
-                self._reverse_enable.value(True)
-                self._forward_enable.value(False)
                 self._reverse_pwm.duty_u16(pwm_value)
                 self._forward_pwm.duty_u16(0)
