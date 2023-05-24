0b8d45d4659515d4d829fd3e06acfe37001699bb
ahmed-ibrahim0 <ahmedibrahim6479@gmail.com>
2023-05-24 13:40:57 +0100

    Corrected spitoL2S2 code

--- L2S2_test/L2S2.py
+++ L2S2_test/L2S2.py
@@ -2,18 +2,18 @@
 FOR MICROPYTHON (TO BE RUN ON PICO)
 '''
 
-from L2S2SPITest_rainbow import spiToL2S2
+import L2S2SPITest_rainbow as L2S2
 
 # Test data
 GPS_longitude = "69"
 
 # Configure wifi
 payload_set_wifi = bytearray('AndroidAP|ahmed123'.encode("utf-8"))
-spiToL2S2(5, payload_set_wifi)
+L2S2.spiToL2S2(5, payload_set_wifi)
 
 # Configure field ID
 def get_field_id(record_id, plate_template_id, control_id):
-    return record_id + "|" + plate_template_id + "|" + control_id
+    return record_id + "|" + plate_template_id + "|" + control_id + "\0"
 
 record_id = "110" # Patient: Minnie Mouse
 plate_template_id = "6e0485b5-cd17-4438-aff8-afe0578ed71f" # Plate: Group_3_Production
@@ -22,16 +22,26 @@ control_id = "4" # Textbox: GPSLongitude
 field_id = get_field_id(record_id, plate_template_id, control_id)
 
 # Configure payload
-def get_payload_str(field_id, type, content, units = None):
+def get_payload_int(field_id, type, content, units = None):
     '''
     See Page 12 of "Instrument to MMDC" document for type code
     '''
-    _type = bytearray(1)
-    _type[0] = type
-    payload =  bytearray(field_id.encode("utf-8")) + _type + bytearray(content.encode("utf-8")) + bytearray(units.encode("utf-8"))
+    __field__ = bytearray(field_id.encode("utf-8"))
+    
+    if type == 2:
+        __type__ = bytearray([0x02])
+        # __type__ = bytearray(1)
+        # __type__[0] = 2
+    else:
+        print("TypeError: Incorrect type for function")
+    
+    __content__ = content.to_bytes(4,"little")
+    __units__ = bytearray(units.encode("utf-8"))
+
+    payload =  __field__ + __type__ + __content__ + __units__
     return payload
 
-payload_test = get_payload_str(field_id, type = 5, content = GPS_longitude, units = "degrees")
+payload_test = get_payload_int(field_id, type = 5, content = GPS_longitude, units = "degrees")
 
 # Send data
-spiToL2S2(150, payload = payload_test)
+L2S2.spiToL2S2(150, payload = payload_test)
