4490947cb5aec2c627eeb112d783c23290f5439f
ahmed-ibrahim0 <ahmedibrahim6479@gmail.com>
2023-05-26 12:21:00 +0100

    Crash alarm tuning

--- Sensor_Data_Collection/scripts/crash_prevention.py
+++ Sensor_Data_Collection/scripts/crash_prevention.py
@@ -3,22 +3,24 @@ import utime as time
 def start_crash_prevention(distance_sensor, motor, speaker, current_total_crashes, MA_order=3, time_step=1, DEBUG=True):  
     while True:
         distance = distance_sensor.get_smooth_distance(MA_order)
-        gap_width = 0.004*distance # maps distance to period between speaker pulses in seconds
 
         if DEBUG:
             print("Distance:", distance, "mm")
 
         if distance < 500: # and low velocity
+            gap_width = 0.002*distance # maps distance to period between speaker pulses in seconds
             speaker.play(tune = 750, duration = 0.1)
-            time.sleep(gap_width)
-
-        if distance < 500: # and high velocity
+            t = gap_width
+        elif distance < 500: # and high velocity
             motor.disable()
             time.sleep(5)
             motor.enable()
             current_total_crashes += 1 # Records the number of actual crashes and near misses
+            t = time_step
             # ADD CODE TO TURN ON BRAKES
+        else:
+            t = time_step
             
     
-        time.sleep(time_step)
+        time.sleep(t)
 
