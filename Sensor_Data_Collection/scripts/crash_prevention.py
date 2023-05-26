ab933ca5611ea0c6c87304e478742e693ab2cfc4
ahmed-ibrahim0 <ahmedibrahim6479@gmail.com>
2023-05-26 19:58:01 +0100

    Adjusted joystick gain when close to an object

--- Sensor_Data_Collection/scripts/crash_prevention.py
+++ Sensor_Data_Collection/scripts/crash_prevention.py
@@ -1,6 +1,7 @@
 import utime as time
 from collections import deque
 
+
 def joystick_change(joystick_y_positions):
     '''
     Args: array, queue and array for three joystick positions in the range -1 to 1
@@ -9,10 +10,17 @@ def joystick_change(joystick_y_positions):
 
     return abs(joystick_y_positions[0] - joystick_y_positions[1]) + abs(joystick_y_positions[1] - joystick_y_positions[2])
 
-def start_crash_prevention(distance_sensor, motor, speaker, joystick, current_total_crashes, MA_order=3, time_step=1, DEBUG=True):
+def start_crash_prevention(distance_sensor, motor, speaker, joystick, MA_order=3, time_step=1, DEBUG=True):
 
     # Initialise queue for joystick data. Assumption: wheelchair takes 3s to reach steady-state velocity when joystick is fully forward
     joystick_y_positions = deque([0,0,0])
+
+    # Initialise counter for total crashes (number of actual crashes and near misses)
+    global current_total_crashes
+    current_total_crashes = 0
+
+    # Load posSpeedAmplitude (i.e. joystick gain as a global variable)
+    global posSpeedAmplitude
     
     while True:
         distance = distance_sensor.get_smooth_distance(MA_order)
@@ -28,15 +36,17 @@ def start_crash_prevention(distance_sensor, motor, speaker, joystick, current_to
             gap_width = 0.002*distance # maps distance to period between speaker pulses in seconds
             speaker.play(tune = 750, duration = 0.1)
             t = gap_width
+            posSpeedAmplitude = distance/500 # scale joystick gain according to distance from object
         elif distance < 500 and stick_change < 0.1 and joystick_y_positions[2] > 0.975 :
             motor.disable()
             time.sleep(5)
             motor.enable()
-            current_total_crashes += 1 # Records the number of actual crashes and near misses
+            current_total_crashes += 1
             t = time_step
             # ADD CODE TO TURN ON BRAKES
         else:
             t = time_step
+            posSpeedAmplitude = 1
             
     
         time.sleep(t)
