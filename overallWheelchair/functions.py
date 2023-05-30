9c9241ede8ed3d8f54cadbdd4848c49a6025b24d
Ashwin Gunasekaran <ag2169@cam.ac.uk>
2023-05-30 12:06:14 +0100

    added warning system

--- overallWheelchair/functions.py
+++ overallWheelchair/functions.py
@@ -4,6 +4,8 @@ All functions that are used in main.py are stored here
 
 """
 
+
+
 # import libraries
 from collections import deque
 from picozero import Speaker
@@ -15,6 +17,7 @@ def startCrashPrevention(distance, speed, speed_threshold = 1, beep_width = 100)
     global speedAmplitude
     global angSpeedAmplitude
     global stopSignal
+    global stopDuration
 
     global parking
     global cur_time
@@ -38,6 +41,7 @@ def startCrashPrevention(distance, speed, speed_threshold = 1, beep_width = 100)
 
     elif distance < 500 and speed > speed_threshold:
         stopSignal = -1
+        stopDuration = 5
         if not recent_crash:
             current_total_crashes += 1
         parking = False
@@ -56,8 +60,34 @@ def startCrashPrevention(distance, speed, speed_threshold = 1, beep_width = 100)
 def startCrashDetection():
     pass
 
-def joyStickPullBack():
-    pass
+def joyStickPullBack(ypos,freq):
+    """
+    When given in a list of previous y positions at least for 2 seconds then detect if sudden pull back 
+
+    :param @ypos: list of previous speed positions on joystick
+    :out binary signal (-1,0) and stopping 
+    if sharp pull back detected then a -1 is sent
+    or button pressed
+    """
+    deltaT = 1 / freq
+    cutoff = 0.5
+    # detect if curr pos is less than -0.5
+    # also rate of change must be big 
+    # in last second must value must be greater than 0.5
+    ypos = list(ypos)
+    currPosTest  = (np.average(ypos[-10:-1]) < -0.5 )
+    rate = (ypos[-1] - ypos[-(freq+1)])/(deltaT * freq )
+    #print("rate={}".format(rate))
+    rateHigh = (rate < cutoff)
+    lastSecond = ypos[-(int(freq/2)):-1]
+    highestValue = max(lastSecond)
+    #print("highest value = {}".format(highestValue))
+    lastSecondPositive = highestValue > 0.3
+    #print(currPosTest,rateHigh,lastSecondPositive)
+    if currPosTest and rateHigh and lastSecondPositive:
+        return -1,2
+    else:
+        return 0,0
 
 
 # filtering and tremor tracking
