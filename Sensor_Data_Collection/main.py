877bfd2f9472a63241585cbe1d8c42b70b121326
Rehan Sheikh <rs2126@cam.ac.uk>
2023-05-30 20:09:22 +0100

    Added robust crash detection

--- Sensor_Data_Collection/main.py
+++ Sensor_Data_Collection/main.py
@@ -128,11 +128,17 @@ def joystick_isr(timer):
 
 
 
-
-
-
 # Data Processing ISR
 def crash_detection_isr(timer):
+    global last_crash_time
+    cool_down_period = 2.0  # Time in seconds
+
+    current_time = time.ticks_ms()/1000  # Time in seconds
+
+    # If not enough time has passed since the last crash, return immediately
+    if last_crash_time is not None and (current_time - last_crash_time) < cool_down_period:
+        return
+
     accel_data = sensor_data["accel"]
     distance = sensor_data["distance"]
     global current_true_crashes
@@ -140,15 +146,17 @@ def crash_detection_isr(timer):
         acc_mag = math.sqrt(accel_data.AccX**2 + accel_data.AccY**2)
         jerk = None
         if previous_data["accel"] is not None:
-            prev_acc_mag = math.sqrt(previous_data["accel"].AccX**2 + previous_data["accel"].AccY**2 )
-            jerk = (acc_mag - prev_acc_mag) / (time_crash_detection/1000) # Assuming this ISR runs every 1s
+            prev_acc_mag = math.sqrt(previous_data["accel"].AccX**2 + previous_data["accel"].AccY**2)
+            jerk = (acc_mag - prev_acc_mag) / (time_crash_detection/1000)  # Assuming this ISR runs every 1s
         # Update previous data
         previous_data["accel"] = accel_data
 
         if jerk > 50 and acc_mag > 6:  # Add gyro condition if needed
             current_true_crashes += 1
+            last_crash_time = current_time  # Update the time of the last crash
             if DEBUG:
                 print("Crash Detected")
+
                 
 # L2S2 Data Sending ISR
 def L2S2_isr(timer):
